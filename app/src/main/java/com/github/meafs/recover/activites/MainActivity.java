961e494b1ccf561e998ee4c5c956aafae68065c4
Abrar Fahim <abrar.fahim@yandex.com>
2022-01-01 01:43:32 +0600

    Working on the walkthrough

--- app/src/main/java/com/github/meafs/recover/activites/MainActivity.java
+++ app/src/main/java/com/github/meafs/recover/activites/MainActivity.java
@@ -1,57 +1,60 @@
 package com.github.meafs.recover.activites;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import com.github.meafs.recover.R;
-import com.github.meafs.recover.utils.Alert;
+import com.shashank.sony.fancywalkthroughlib.FancyWalkthroughActivity;
+import com.shashank.sony.fancywalkthroughlib.FancyWalkthroughCard;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private Alert alert;
-    private Button button;
-    private SharedPreferences sharedPreferences;
+public class MainActivity extends FancyWalkthroughActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        alert = new Alert(this, "Hello","Ok","Back");
-//        alert.CreateAlert().show();
-        button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, SettingActivity.class));
-            }
-        });
 
-        Button button2 = findViewById(R.id.button2);
+        FancyWalkthroughCard card1 = new FancyWalkthroughCard("Unique QR code login", "", R.drawable.first);
+        FancyWalkthroughCard card2 = new FancyWalkthroughCard("Online lessons and quizzes", "", R.drawable.second);
+        FancyWalkthroughCard card3 = new FancyWalkthroughCard("Video demonstration of first aid", "", R.drawable.third);
 
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    sharedPreferences = getSharedPreferences("Settings", Context.MODE_PRIVATE);
+        card1.setBackgroundColor(R.color.white);
+        card1.setIconLayoutParams(300, 300, 0, 0, 0, 0);
+        card1.setDisplaySkip(false);
+        card2.setBackgroundColor(R.color.white);
+        card2.setIconLayoutParams(300, 300, 0, 0, 0, 0);
+        card2.setDisplaySkip(false);
 
-                    int savedWeight = sharedPreferences.getInt("weight", -1);
-                    String savedUnit = sharedPreferences.getString("unit", null);
-                    String savedGender = sharedPreferences.getString("sex", null);
+        card3.setBackgroundColor(R.color.white);
+        card3.setIconLayoutParams(300, 300, 0, 0, 0, 0);
+        card3.setDisplaySkip(false);
 
-                    Toast.makeText(MainActivity.this, "Saved weight: " + savedWeight + " " + savedUnit + " saved gender: " + savedGender, Toast.LENGTH_SHORT).show();
-                } catch (Exception ex) {
-                    Toast.makeText(MainActivity.this, "Null", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        List<FancyWalkthroughCard> pages = new ArrayList<>();
+
+        pages.add(card1);
+        pages.add(card2);
+        pages.add(card3);
+
+        for (FancyWalkthroughCard page : pages) {
+            page.setTitleColor(R.color.black);
+            page.setDescriptionColor(R.color.black);
+        }
+        setFinishButtonTitle("Get Started");
+
+        showNavigationControls(true);
+        setColorBackground(R.color.colorGreen);
+        setInactiveIndicatorColor(R.color.grey_600);
+        setActiveIndicatorColor(R.color.colorGreen);
+        setOnboardPages(pages);
 
 
+    }
+
+    @Override
+    public void onFinishButtonPressed() {
+        Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
     }
 }
