af7f11a34863707b3a0cd057a62fcfa62eeecec7
istiakshihab <istiak@pm.me>
2022-01-22 14:18:52 +0600

    Dashboard Added

--- app/src/main/java/com/github/meafs/recover/activites/ContentActivity.java
+++ app/src/main/java/com/github/meafs/recover/activites/ContentActivity.java
@@ -1,6 +1,7 @@
 package com.github.meafs.recover.activites;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,23 +14,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.github.meafs.recover.R;
 import com.github.meafs.recover.adapters.ContentRecylerAdapter;
 import com.github.meafs.recover.models.ContentModel;
 import com.github.meafs.recover.viewmodels.ContentViewModel;
+import com.google.android.material.appbar.MaterialToolbar;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class ContentActivity extends AppCompatActivity {
 
     private ContentViewModel contentViewModel;
     private ArrayList<ContentModel> list;
     private RecyclerView recyclerView;
-    private ImageView button;
-
+    private MaterialToolbar toolbar;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,42 +41,35 @@ public class ContentActivity extends AppCompatActivity {
 
         contentViewModel = ViewModelProviders.of(this).get(ContentViewModel.class);
         contentViewModel.init(pref.getString("authToken", ""));
+        toolbar = findViewById(R.id.toolbar);
+        progressBar = findViewById(R.id.progressBar);
 
-        button = findViewById(R.id.back);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ContentActivity.this, MainActivity.class));
-
-            }
-        });
+        toolbar.setNavigationOnClickListener(view -> startActivity(new Intent(ContentActivity.this, MainActivity.class)));
 
         recyclerView = findViewById(R.id.recylerview);
         list = new ArrayList<>();
 
-        contentViewModel.getContentResponseLiveData().observe(this, new Observer<List<ContentModel>>() {
-            @Override
-            public void onChanged(List<ContentModel> contactModels) {
-                list.addAll(contactModels);
-                System.out.println(contactModels.size());
+        contentViewModel.getContentResponseLiveData().observe(this, contactModels -> {
+            list.addAll(contactModels);
+            System.out.println(contactModels.size());
 
-            }
         });
 
-        new Handler().postDelayed(new Runnable() {
-            public void run() {
-                System.out.println(list.size());
-                try {
-                    ContentRecylerAdapter contentRecylerAdapter = new ContentRecylerAdapter(list, ContentActivity.this);
-                    contentRecylerAdapter.setList(list);
+        new Handler().postDelayed(() -> {
+            System.out.println(list.size());
+            try {
+                ContentRecylerAdapter contentRecylerAdapter = new ContentRecylerAdapter(list, ContentActivity.this);
+                contentRecylerAdapter.setList(list);
 
-                    recyclerView.setAdapter(contentRecylerAdapter);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(ContentActivity.this));
-                } catch (Exception e) {
-                    Toast.makeText(ContentActivity.this, "Error! Please connect to internet!", Toast.LENGTH_SHORT).show();
-                }
+                progressBar.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
 
+                recyclerView.setAdapter(contentRecylerAdapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(ContentActivity.this));
+            } catch (Exception e) {
+                Toast.makeText(ContentActivity.this, "Error! Please connect to internet!", Toast.LENGTH_SHORT).show();
             }
+
         }, 3000);
     }
 }
