85597dd78268e2587fc4776c5685262d78e7bb05
istiakshihab <istiak@pm.me>
2022-02-04 13:40:16 +0600

    Disease Screening Lib Added

--- app/src/main/java/com/github/meafs/recover/activites/DiseaseScreeningActivity.java
+++ app/src/main/java/com/github/meafs/recover/activites/DiseaseScreeningActivity.java
@@ -4,24 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
 import com.github.meafs.recover.R;
 import com.github.meafs.recover.databinding.ActivityDiseaseScreeningBinding;
-import com.github.meafs.recover.models.Symptopm;
-import com.pchmn.materialchips.ChipsInput;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class DiseaseScreeningActivity extends AppCompatActivity {
     private static final String TAG = "ScreeningActivity";
