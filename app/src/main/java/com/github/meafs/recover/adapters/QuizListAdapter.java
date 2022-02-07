ab70c5f7529dba5fca8f348cbd698080e3cfb6cc
istiakshihab <istiak@pm.me>
2022-02-07 12:08:15 +0600

    Quiz Activity UI Rework

--- app/src/main/java/com/github/meafs/recover/adapters/QuizListAdapter.java
+++ app/src/main/java/com/github/meafs/recover/adapters/QuizListAdapter.java
@@ -3,6 +3,7 @@ package com.github.meafs.recover.adapters;
 import android.content.Context;
 import android.content.Intent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,9 @@ import com.github.meafs.recover.R;
 import com.github.meafs.recover.activites.QuizRunnerActivity;
 import com.github.meafs.recover.models.QuizModel;
 import com.github.meafs.recover.utils.Speak;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
 
@@ -47,7 +51,8 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.ViewHo
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final QuizModel quizModel = quizModels.get(position);
-        holder.textView.setText(quizModel.getTitle());
+        holder.textView.setText(quizModel.getTitle().trim());
+        holder.tvDuration.setText(quizModel.getQuizzes().size()+" Questions");
         holder.constraintLayout.setOnClickListener(view -> {
             Speak speak = new Speak(context);
             speak.speak(ttsObject, holder.textView.getText().toString());
@@ -55,21 +60,41 @@ public class QuizListAdapter extends RecyclerView.Adapter<QuizListAdapter.ViewHo
             intent.putStringArrayListExtra("quizStrings", new ArrayList<>(quizModel.getQuizzes()));
             context.startActivity(intent);
         });
+
+        String[] tags = quizModel.getTags().split(",");
+        generateChips(tags, holder);
     }
 
+    public void generateChips(String[] tags, ViewHolder holder){
+        for (String tag : tags) {
+            Chip chip = new Chip(context);
+            ChipDrawable chipDrawable = ChipDrawable.createFromAttributes(context, null, 0, R.style.MaterialComponents_Chip_Thin);
+            chip.setChipDrawable(chipDrawable);
+            chip.setText(tag.trim());
+            chip.setOnClickListener(view -> {
+                Speak speak = new Speak(context);
+                speak.speak(ttsObject, tag);
+            });
+            holder.chipGroup.addView(chip);
+        }
+    }
     @Override
     public int getItemCount() {
         return quizModels.size();
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView textView;
-        public ConstraintLayout constraintLayout;
+        private TextView textView;
+        private TextView tvDuration;
+        private ChipGroup chipGroup;
+        private ConstraintLayout constraintLayout;
 
         public ViewHolder(View itemView) {
             super(itemView);
             this.textView = itemView.findViewById(R.id.quiz_name);
-            constraintLayout = itemView.findViewById(R.id.quiz_list_layout);
+            this.tvDuration = itemView.findViewById(R.id.tv_duration);
+            this.constraintLayout = itemView.findViewById(R.id.quiz_list_layout);
+            this.chipGroup = itemView.findViewById(R.id.chip_group);
         }
     }
 }
